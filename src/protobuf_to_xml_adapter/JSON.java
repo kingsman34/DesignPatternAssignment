883201bf45842b97230fc7fd3e07f876cb992f8a
Hamim Rahman <hamimr3823@gmail.com>
2023-08-08 22:37:21 +0600

    ProtobufToXmlAdapter

---
+++ src/protobuf_to_xml_adapter/JSON.java
@@ -0,0 +1,9 @@
+package protobuf_to_xml_adapter;
+
+public class JSON {
+    public JSON(){};
+    public JSON(String data){}
+    XML convertToXML(){
+        return new XML("Stringified JSON data");
+    }
+}
